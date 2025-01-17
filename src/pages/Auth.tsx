945f5f533d03b530cb0c54e88f59abc221d3208d
hazimzaman <zamanhazim1922@gmail.com>
2025-01-17 18:20:55 +0500

    8

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -74,7 +74,7 @@ function Auth() {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/clients`
+          redirectTo: window.location.origin + '/clients'
         }
       });
 
